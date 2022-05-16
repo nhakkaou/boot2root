5bd941ac9e4cc413d3951b8a25a035e1c3f36abd
Noreddine Hakkaoui <nhakkaou@e2r5p7.1337.ma>
2022-05-16 21:08:16 +0100

    First Commit

---
+++ script.sh
@@ -0,0 +1,22 @@
+input="./wordlist-url.txt"
+while IFS= read -r line
+do
+   R=$(curl -k -I "https://10.12.100.196/$line" 1&>2 /dev/null | grep "HTTP/1.1 200 OK")
+   echo $R
+	if [ "$R" == 1 ]
+		then
+      echo "Password is : "
+	  tput setaf 2
+	  echo $line
+	  break
+	fi
+done < "$input"
+
+    # url="https://$ipAdress/$line"
+    # echo $url;
+    # resultStatus=`curl -k -I "$url" 1&>2 /dev/null | grep HTTP `
+    # echo $resultStatus;
+    # if [ "$resultStatus" == "200" ]; then
+    #     echo "$line"
+    #     break;
+    # fi
