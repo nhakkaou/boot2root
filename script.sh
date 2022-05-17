a2b97c3abbad6bec42bea67f9d0e199a9d10ad62
Noreddine Hakkaoui <nhakkaou@e2r3p14.1337.ma>
2022-05-17 20:57:41 +0100

    Update

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 input="./wordlist-url.txt"
 while IFS= read -r line
 do
-   R=$(curl -k -I "https://10.12.100.196/$line" 1&>2 /dev/null | grep "HTTP/1.1 200 OK")
+   R=$(curl -k -I "https://10.12.100.216/$line" 1&>2 /dev/null | grep "HTTP/1.1 200 OK")
    echo $R
 	if [ "$R" == 1 ]
 		then
